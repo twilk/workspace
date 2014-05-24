efc7e3ba51dc8ecb6af6555770cbeac91ff1c3c3
twilk <twilk@sigma.ug.edu.pl>
2014-05-24 19:33:01 +0200

    d

--- App4/src/swing/mvc/Main.java
+++ App4/src/swing/mvc/Main.java
@@ -4,10 +4,10 @@ public class Main {
 	public static void main(String[] args) 
 	{
 
-		Model model       = new Model();
-		Widok widok = new Widok(model);  
-		widok.setVisible(true);
-		Controler controler = new Controler(widok, model);
+	//	Model model       = new Model();
+	//	Widok widok = new Widok(model);  
+	//	widok.setVisible(true);
+	//	Controler controler = new Controler(widok, model);
     }
     
 }
